f1b49b010b0aa0896945128728f688873270a483
oukadou753-rgb <oukadou753@gmail.com>
2026-03-06 15:46:50 +0900

    WF_ConfigUI.js を更新

--- WidgetFramework/WF_ConfigUI.js
+++ WidgetFramework/WF_ConfigUI.js
@@ -1,6 +1,9 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-gray; icon-glyph: magic;
+/**
+ * WF_ConfigUI
+ **/
 module.exports = {
 
   async present(activeCfg, profileEngine, activeProfile) {
