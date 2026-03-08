d77634bac8e34f9cc14e4c35938b49dd4a0ee17a
oukadou753-rgb <oukadou753@gmail.com>
2026-03-08 10:37:13 +0900

    WF_AppCore.js を更新

--- WidgetFramework/WF_AppCore.js
+++ WidgetFramework/WF_AppCore.js
@@ -4,7 +4,7 @@
 /**
  * WF_AppCore
  **/
-const WF_CoreBase = importModule("WF_CoreBase")
+const WF_CoreBase = importModule("WidgetFramework/WF_CoreBase")
 const StorageEngine = importModule("WidgetFramework/WF_StorageEngine")
 const WidgetRenderer = importModule("WidgetFramework/WF_WidgetRenderer")
 const MenuEngine = importModule("WidgetFramework/WF_MenuEngine")
