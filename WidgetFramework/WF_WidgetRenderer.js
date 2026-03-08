af10a56617ecfcfec0e731eac9ed7e9c7c2b589a
oukadou753-rgb <oukadou753@gmail.com>
2026-03-08 16:09:23 +0900

    WF_WidgetRenderer.js を更新

--- WidgetFramework/WF_WidgetRenderer.js
+++ WidgetFramework/WF_WidgetRenderer.js
@@ -24,7 +24,7 @@ module.exports = class WF_WidgetRenderer {
     const layout = cfg.layout || {}
 
     // 背景
-log("values.bgColorTop: " + values.bgColorTop)
+log("values: " + JSON.stringify(values, null, 2))
     if (values.bgColorTop && values.bgColorBottom) {
       await this.setGradientBackground(widget, values.bgColorTop, values.bgColorBottom)
     }
