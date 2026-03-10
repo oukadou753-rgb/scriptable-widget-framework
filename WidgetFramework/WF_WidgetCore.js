7570722accf962a9274c6e07ab3c092614ed1990
oukadou753-rgb <oukadou753@gmail.com>
2026-03-10 10:25:30 +0900

    WF_WidgetCore.js を更新

--- WidgetFramework/WF_WidgetCore.js
+++ WidgetFramework/WF_WidgetCore.js
@@ -4,7 +4,7 @@
 /**
  * WF_WidgetCore
  **/
-const { loadModule } = importModule("ModuleLoader")
+// const { loadModule } = importModule("ModuleLoader")
 
 const WF_CoreBase = importModule("WidgetFramework/WF_CoreBase")
 const StorageEngine = importModule("WidgetFramework/WF_StorageEngine")
