14ea641d988a67000f5a68d395eff468b1ad56fb
oukadou753-rgb <oukadou753@gmail.com>
2026-03-19 06:50:57 +0900

    WF_CoreBase.js を更新

--- WidgetFramework/WF_CoreBase.js
+++ WidgetFramework/WF_CoreBase.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: orange; icon-glyph: magic;
+// icon-color: deep-gray; icon-glyph: magic;
 /**
  * WF_CoreBase
  * UTF-8 日本語コメント
