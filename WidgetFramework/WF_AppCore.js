82dfd2070941540ad36247c01a8eb238cdb4e935
oukadou753-rgb <oukadou753@gmail.com>
2026-03-20 17:26:07 +0900

    WF_AppCore.js を更新

--- WidgetFramework/WF_AppCore.js
+++ WidgetFramework/WF_AppCore.js
@@ -45,7 +45,7 @@ module.exports = class WF_AppCore {
     this.notificationUI = WF_NotificationUI
     this.notification.syncStatus()
 
-    this.notificationHandlers = modules.notificationHandlers(this)
+    this.notificationHandlers = moduleCache.notificationHandlers(this)
 
     const core = new WF_CoreBase(appInfo, appConfig, moduleCache)
     WF_CoreBase.mixinCore(this, core)
