0cc69fcd6168291b181fb2ca47e83bda17d10d8c
oukadou753-rgb <oukadou753@gmail.com>
2026-03-28 16:45:54 +0900

    WF_NotificationHandlers.js を更新

--- WidgetFramework/WF_NotificationHandlers.js
+++ WidgetFramework/WF_NotificationHandlers.js
@@ -5,49 +5,82 @@
  * WF_NotificationHandlers
  * UTF-8 日本語コメント
  **/
+
+// =========================
+// Export
+// ========================
 module.exports = (core) => ({
 
+  // =========================
+  // openProfile
+  // ========================
   openProfile: async (info) => {
+
     core.profile.setActive(info.profile)
     await core.preview(info.widgetFamily)
+
   },
 
+  // =========================
+  // openNotificationUI
+  // ========================
   openNotificationUI: async () => {
+
     await core.notificationUI.present(core)
+
   },
 
+  // =========================
+  // openNotificationDetail
+  // ========================
   openNotificationDetail: async (info) => {
+
     await core.notificationUI.present(core, {
       openId: info.id
     })
+
   },
 
+  // =========================
+  // openPreview
+  // ========================
   openPreview: async (info) => {
 
     if (info.type === "image") {
+
       let img
 
       if (info.src.startsWith("http")) {
+
         const req = new Request(info.src)
         img = await req.loadImage()
+
       } else {
+
         img = Image.fromFile(info.src)
+
       }
 
       if (img) QuickLook.present(img)
+
     }
 
     if (info.type === "list") {
+
       const table = new UITable()
 
       for (const item of info.items || []) {
+
         const row = new UITableRow()
         row.addText(String(item))
         table.addRow(row)
+
       }
 
       await table.present(true)
+
     }
+
   }
 
 })
