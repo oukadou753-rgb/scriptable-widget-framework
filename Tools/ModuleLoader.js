3bc9e2ef4a2278e3dc1aae2588a2fdac5476d9d6
oukadou753-rgb <oukadou753@gmail.com>
2026-03-28 21:02:17 +0900

    ModuleLoader.js を更新

--- Tools/ModuleLoader.js
+++ Tools/ModuleLoader.js
@@ -2,198 +2,135 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-gray; icon-glyph: magic;
 /**
- * DevWidget
+ * ModuleLoader
  * UTF-8 日本語コメント
- * 2026/03/21 09:00
- */
-const DEFAULT_APP_ID = "Weather"
-const DEFAULT_STRAGE_TYPE = "local"
+ **/
 
-const APP_DEV_MODE = true
-const APP_ID = args.widgetParameter || DEFAULT_APP_ID
-const APP_VERSION = "1.0.0"
-const APP_CONFIG = `App_${APP_ID}Config`
+// =========================
+// Export
+// =========================
+module.exports = class ModuleLoader {
 
-const WF_MODULE_DIR = "WidgetFramework/"
-const WF_REPO = "oukadou753-rgb/scriptable-widget-framework"
+  constructor(storageType = "local") {
 
-const APP_INFO = {
-  debug: APP_DEV_MODE,
-  id: APP_ID,
-  version: APP_VERSION,
-  storageType: DEFAULT_STRAGE_TYPE,
-  moduleDir: WF_MODULE_DIR,
-  frameworkRepo: WF_REPO,
-}
+    this.storageType = storageType
+    this.cache = {}
 
-const MODULES = {
-  WF_StorageEngine: { type: "both", path: WF_MODULE_DIR },
-  WF_WidgetRenderer: { type: "both", path: WF_MODULE_DIR },
-  WF_ProfileEngine: { type: "both", path: WF_MODULE_DIR },
-  WF_DataProvider: { type: "both", path: WF_MODULE_DIR },
-  WF_NotificationManager: { type: "both", path: WF_MODULE_DIR },
-  WF_CoreBase: { type: "both", path: WF_MODULE_DIR },
+    this.initStorage()
 
-  WF_MenuEngine: { type: "app", path: WF_MODULE_DIR },
-  WF_ConfigUI: { type: "app", path: WF_MODULE_DIR },
-  WF_NotificationHandlers: { type: "app", path: WF_MODULE_DIR },
-  WF_NotificationUI: { type: "app", path: WF_MODULE_DIR },
+    console.log("MODULE LOADER" + "\n")
+    console.log("STORAGE: " + this.storageType)
+    console.log("BASE: " + this.baseDir + "\n")
 
-  WF_AppCore: { type: "app", path: WF_MODULE_DIR },
-  WF_WidgetCore: { type: "widget", path: WF_MODULE_DIR },
+  }
 
-  [APP_CONFIG]: { type: "both", path: "" },
-}
-
-module.exports = {
-  
   // =========================
-  // start
+  // load module
   // =========================
-  async start(appInfo = APP_INFO) {
+  load(path) {
 
     try {
 
-      await this.init(appInfo)
+//       if (this.cache[path]) {
+//         return this.cache[path]
+//       }
 
-    } catch(e) {
+      const fullPath = this.getFilePath(path)
 
-      this.catch(e)
+      const module = importModule(fullPath)
 
-    } finally {
+//       this.cache[fullPath] = module
 
-      this.terminate(appInfo)
+      if (module) console.log("ModuleLoader: " + path)
+
+      return module
 
     }
 
-  },
+    catch(e) {
 
-  // =========================
-  // init
-  // =========================
-  async init(appInfo) {
+      const module = importModule(path)
 
-      const ModuleLoader = importModule("ModuleLoader")
-      const moduleLoader = new ModuleLoader(appInfo.storageType)
+      if (module) console.warn("importModule: " + path)
 
-      let obj = {}
+      return module
 
-      for (key in MODULES) {
-
-        const value = MODULES[key]
-        const path = value.path + key
-        const type = value.type
-
-        if (this.isImport(type)) {
-          try {
-            obj[key] = moduleLoader.load(path)
-          } catch(e) {
-            obj[key] = importModule(key)
-          }
-        }
-
-      }
-
-      if (config.runsInWidget) {
-        const widgetCore = new obj.WF_WidgetCore(appInfo, obj[APP_CONFIG], obj)
-        await widgetCore.start()
-      } else {
-        const appCore = new obj.WF_AppCore(appInfo, obj[APP_CONFIG], obj)
-        await appCore.start()
-      }
-
-  },
-
-  // =========================
-  // catch
-  // =========================
-  catch(e) {
-
-    console.error(e.message)
-    errorWidget(e)
-
-  },
-
-  // =========================
-  // terminate
-  // =========================
-  terminate(appInfo) {
-
-    Script.complete()
-
-  },
-
-  // =========================
-  // isImport
-  // =========================
-  isImport(type) {
-    if (config.runsInWidget) {
-      if(type == "widget" || type == "both") return true
-    } else {
-      if(type == "app" || type == "both") return true
     }
-    return false
-  },
-
-  // =========================
-  // setAppInfo
-  // =========================
-  setAppInfo(key, values) {
-
-    APP_INFO[key] = values
 
   }
 
-}
+  // =========================
+  // resolve path
+  // =========================
+  getFilePath(path) {
 
-// =========================
-// errorWidget
-// =========================
-function errorWidget(e) {
+    const file = path.endsWith(".js") ? path : path + ".js"
 
-  const color = [ Color.black(), Color.red() ]
-  const bgColor = Color.yellow()
+    const fullPath = this.fm.joinPath(this.baseDir, file)
 
-  let w = new ListWidget()
-  w.setPadding(10, 10, 10, 10)
-  w.backgroundColor = bgColor
+    if (!this.fm.fileExists(fullPath)) {
 
-  addText(w, "WidgetFramework", Font.semiboldSystemFont(13), color[0], "left")
-  w.addSpacer()
-  addText(w, "Script Stopped", Font.boldSystemFont(18), color[0], "center")
-  w.addSpacer(10)
-  addText(w, e.message, Font.boldSystemFont(16), color[1], "center")
-  if (e.stack) {
-    w.addSpacer(6)
-    addText(w, e.stack.slice(0, 120), Font.systemFont(10), color[1], "left")
+      throw new Error("Module not found:\n" + fullPath)
+
+    }
+
+    // iCloud auto download
+    if (this.storageType === "icloud") {
+
+      if (!this.fm.isFileDownloaded(fullPath)) {
+
+        this.fm.downloadFileFromiCloud(fullPath)
+
+      }
+
+    }
+
+    return fullPath
+
   }
-  w.addSpacer()
-  addText(w, APP_INFO.storageType + " " + new Date().toLocaleTimeString(), Font.semiboldSystemFont(11), color[1], "right")
 
-  if (config.runsInWidget) Script.setWidget(w)
-  else w.presentLarge()
+  // =========================
+  // initStorage
+  // =========================
+  initStorage(){
 
-}
+    switch (this.storageType) {
 
-// =========================
-// addText
-// =========================
-function addText(w, text, font, color, aling) {
+      case "icloud":
+        this.fm = FileManager.iCloud()
+        this.baseDir = this.fm.documentsDirectory()
+        break
 
-  let t = w.addText(text)
-  if (font) t.font = font
-  if (color) t.textColor = color
-  if (aling == "right") t.rightAlignText()
-  else if (aling == "center") t.centerAlignText()
+      case "bookmark":
+        this.fm = FileManager.local()
+        this.baseDir = this.fm.bookmarkedPath("Scriptable")
+        break
 
-}
+      default:
+        this.fm = FileManager.local()
+        this.baseDir = this.fm.documentsDirectory()
+
+    }
+
+    this._ensureDirs()
+
+  }
   
-// =========================
-// Module Test
-// =========================
-const module_name = module.filename.match(/[^\/]+$/ )[ 0 ].replace('.js', '');
-if (module_name == Script.name()) {
-  (async() => {
-    await module.exports.start()
-  })()
+  // =========================
+  // _ensureDirs
+  // =========================
+  _ensureDirs() {
+
+  }
+
+  // =========================
+  // static shortcut
+  // =========================
+  static import(path, storageType = "local") {
+
+    const loader = new ModuleLoader(storageType)
+
+    return loader.load(path)
+  }
+
 }
