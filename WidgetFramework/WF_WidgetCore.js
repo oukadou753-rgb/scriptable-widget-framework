f968f690506fe069d6b234a904115bf231a7e4f8
oukadou753-rgb <oukadou753@gmail.com>
2026-04-01 10:20:09 +0900

    WF_WidgetCore.js を更新

--- WidgetFramework/WF_WidgetCore.js
+++ WidgetFramework/WF_WidgetCore.js
@@ -65,32 +65,3 @@ module.exports = class WF_WidgetCore {
 
   }
 }
-    this.notification.syncStatus()
-
-    const core = new WF_CoreBase(appInfo, appConfig, moduleCache)
-    WF_CoreBase.mixinCore(this, core)
-  }
-
-  async start() {
-/*
-    // Framework update check
-    const update = await this.checkFrameworkUpdate()
-
-    if (update?.update) {
-
-      console.log(
-        "Framework Update Available\n" +
-        "Local: " + update.local + "\n" +
-        "Remote: " + update.remote
-      )
-
-    }
-*/
-
-    const widget = await this.run()
-    Script.setWidget(widget)
-    Script.complete()
-    return
-
-  }
-}
